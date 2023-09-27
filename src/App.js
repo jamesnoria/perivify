592ab257b276d95dce6a65c4e164022ad37b412a
James Noria <jnoria.dev@gmail.com>
2023-09-26 19:57:57 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Button } from 'semantic-ui-react';
+import { getAuth } from 'firebase/auth';
 
 function App() {
+  const [user, setUser] = useState(undefined);
+
+  if (user === undefined) {
+    const auth = getAuth();
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hi, I'm a React App</h1>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
     </div>
   );
 }
---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXsRgzPtW8HZ3-_oIaIdKYCYbM4OTdpZI",
+  authDomain: "peruvify.firebaseapp.com",
+  projectId: "peruvify",
+  storageBucket: "peruvify.appspot.com",
+  messagingSenderId: "122185424439",
+  appId: "1:122185424439:web:a77c643b0665d4b638e9b2"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
